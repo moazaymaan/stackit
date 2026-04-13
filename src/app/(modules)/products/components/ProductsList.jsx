0660ea64b304660cf0dfd3be44b325d4ad05dd73
moazaymaan <moazaymans211@gmail.com>
2026-04-13 20:58:53 +0200

    Add console log for debugging in ProductsList component

--- src/app/(modules)/products/components/ProductsList.jsx
+++ src/app/(modules)/products/components/ProductsList.jsx
@@ -3,7 +3,8 @@
 // Purpose: This module handles products logic and UI.
  // It fetches products data and renders a table of products.
 
- 
+ console.log("ProductsList component loaded");
+
 import { useProducts } from "../hooks/useProducts";
 
 // Render the main products component.
